735bd11155bb2c78ccda39a0549c24643914adf8
Walker Le <walker.le88@gmail.com>
2023-01-27 22:54:10 -0700

    commit tree e6404b0527d766d56eee6916ed0cd67e8f9b5960

--- app.rb
+++ app.rb
@@ -14,10 +14,29 @@
 # # 1 + 1
 # puts add + add
 
-def add num1, num2
-    puts num1
-    puts num2
-    num1 + num2
-end
+# def add num1, num2
+#     puts num1
+#     puts num2
+#     num1 + num2
+# end
 
-puts add(1,3) + add(2,2)
+# puts add(1,3)
+# puts add(1,3) + add(2,2)
+
+# app.rb
+# puts "Hello world!"
+
+# print "Hello world!"
+
+# puts [1, 2, 3]
+# p [1, 2, 3]
+
+# pp [{ id: 1, hello: "World"},
+#     { id: 2, hello: "Ruby"},
+#     { id: 3, hello: "Moon"},
+#     { id: 4, hello: "Learner"},
+# ]
+
+puts "Hello World!"
+print "Pass this test, please."
+p [1, 2, 3]
